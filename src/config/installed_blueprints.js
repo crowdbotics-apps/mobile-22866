626613c2d64a7a1aed846525d57355c314e2fff8
Crowdbotics <team@crowdbotics.com>
2020-11-21 18:36:41 +0000

    add package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Settings177121', human_name: 'Settings', access_route: 'Settings177121'},
 { name: 'ArticleList177102', human_name: 'Article List', access_route: 'ArticleList177102'},
 { name: 'ArticleList177101', human_name: 'Article List', access_route: 'ArticleList177101'},
 { name: 'ArticleList177100', human_name: 'Article List', access_route: 'ArticleList177100'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Settings177121Navigator from '../features/Settings177121/navigator';
 import ArticleList177102Navigator from '../features/ArticleList177102/navigator';
 import ArticleList177101Navigator from '../features/ArticleList177101/navigator';
 import ArticleList177100Navigator from '../features/ArticleList177100/navigator';
@@ -16,6 +17,7 @@ import ArticleList177100Navigator from '../features/ArticleList177100/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Settings177121: { screen: Settings177121Navigator },
 ArticleList177102: { screen: ArticleList177102Navigator },
 ArticleList177101: { screen: ArticleList177101Navigator },
 ArticleList177100: { screen: ArticleList177100Navigator },
